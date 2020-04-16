cfef36f6c6f95a67baef905ff117057c2a86732b
dalejoroc11 <diegorojas279@hotmail.com.com>
2020-04-16 20:18:03 +0000

    main

--- main.c
+++ main.c
@@ -1,54 +1,33 @@
 #include "functions.h"
-/**
- *
- *
- *
- */
 
 int main(int argc, char *argv[], char **env)
 {
-        char *cont, *result, *line = NULL;
-        char **tokenizado;
-        int status = 0, val_fd = 0, comp = 0, i = 0;
-        ssize_t bufsize = 0;
-        pid_t hijo;
+  char *cont, *result;
+  char **tokenizado;
+  int status = 0, val_fd = 0, comp = 0, i = 0;
+  ssize_t c;
+  pid_t hijo;
 
-        while (1)
-        {
-               
+  while (1)
+    {
+      hijo = fork();
+      val_fd = isatty(STDIN_FILENO);
+      if (hijo == 0)
+	{
+	  if (argc == 1)
+	    {
+	      write(1, "#cisfun$ ", 10);
+	      cont = read_line(&c);
+	      tokenizado = words(cont, " \t\r\n");
+	      comp = _strcmp(tokenizado[0], "exit");
+	      comp = _strcmp(tokenizado[0], "env");
+	      while (env[i] != NULL)
+		{
+		  write(1, env[i], _strlen(env[i]));
+		  write(1, "\n", 1);
+		  i++;
+		}
 
-                        if (argc == 1)
-                        {
-                               	write(1, "Saroco83-dalejoroc11$ ", 10);
-                                cont = getline(&line, &bufsize, stdin);
-                                }
-                                tokenizado = words(cont, " \t\r\n");
-                                comp = strcmp(tokenizado[0], "exit");
-                                comp = strcmp(tokenizado[0], "env");
-                                if (comp == 0)
-                                {
-                                        while (env[i] != NULL)
-                                        {
-                                                write(1, env[i], _strlen(env[i]));
-                                                write(1, "\n", 1);
-                                                i++;
-                                        }
-                                }
-                                        result = l_path(tokenizado[0], env);
-                        
-                        }
-                }
-                else
-                {
-                        wait(&status);
-                        status = WEXITSTATUS(status);
-			            if (status == 2)
-				                exit(127);
-                        if (val_fd == 0 || status == 3)
-                        {
-                                break;
-                        }
-                }
-        }
-        return (0);
-}
+	      result = l_path(tokenizado[0], env);
+	    }
+	}
