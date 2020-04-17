7cd2c08b88a16b03f4bb9d958b27aa13c5844d1c
Saroco83 <estudialondres@gmail.com>
2020-04-17 05:09:16 +0000

    readline

---
+++ _read_line.c
@@ -0,0 +1,15 @@
+#include "functions.h"
+/**
+ * _read_line - Funtion that will read the input
+ * @c: This will be a flag to identify an exit
+ * Return: this will be the previously read line
+ */
+char *_read_line(ssize_t *c)
+{
+	char *input = NULL;
+	size_t bufsize = 0;
+
+	*c = getline(&input, &bufsize, stdin);
+	return (input);
+}
+~
