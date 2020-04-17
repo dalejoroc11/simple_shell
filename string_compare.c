bf6c6d6076ebf42c8b8beab43182ebf89e9c1bdc
Saroco83 <estudialondres@gmail.com>
2020-04-17 05:12:51 +0000

    stringcompare

---
+++ string_compare.c
@@ -0,0 +1,18 @@
+#include "functions.h"
+/**
+ * string_compare - This will compare a two strings
+ * @s1: The first string to compare
+ * @s2: The second string to compare
+ * Return: will return 0 if both are the same or a number if it fails
+ */
+int string_compare(char *s1, char *s2)
+{
+	int i = 0;
+
+	while ((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i])
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+~
